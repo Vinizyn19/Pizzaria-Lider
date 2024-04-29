f7d01507c569f1c8dac589fb2ad9fc524ce10575
viniciuscanavieira <vinicius.teixeirapix@gmail.com>
2024-04-28 22:32:07 -0300

    atualização!

---
+++ script.js
@@ -0,0 +1,17 @@
+let show = true;
+const menuContent = document.querySelector('.content');
+const menuToggle = menuContent.querySelector('.menu-toggle');
+
+menuToggle.addEventListener('click', () => {
+    menuContent.classList.toggle("on", show)
+    show = !show;
+});
+
+  document.getElementById('pedirfrango').addEventListener('click', function() {
+    // Substitua 'outraTela.html' pelo nome do arquivo da outra tela na mesma pasta
+    window.location.href = 'carrinhofrango.html';
+  });
+  document.getElementById('pedirfrango').addEventListener('click', function() {
+    // Substitua 'outraTela.html' pelo nome do arquivo da outra tela na mesma pasta
+    window.location.href = 'carrinhofrango.html';
+  });
